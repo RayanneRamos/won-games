7c4ad506efbe8f9a568dbf5eaf8a55ccbc18c5c1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-03-28 09:58:57 -0300

    logo parte 6: criando uma outra prop de logo usando o tdd

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { render, RenderResult } from '@testing-library/react'
 import theme from '@/styles/theme'
-import { ReactNode } from 'react'
 
-export const renderWithTheme = (children: ReactNode): RenderResult =>
+export const renderWithTheme = (children: React.ReactNode): RenderResult =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
