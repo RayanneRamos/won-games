c7e995ab7059828780b4e637f72971b7cc6b8d9e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-04-03 16:55:38 -0300

    menu parte 8: ajustando o menu para desktop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import * as S from './styles'
 import Logo from '../Logo'
 import { useState } from 'react'
 import Button from '../Button'
+import MediaMatch from '../MediaMatch'
 
 export type MenuProps = {
   userName?: string
@@ -17,12 +18,20 @@ const Menu = ({ userName }: MenuProps) => {
 
   return (
     <S.Wrapper>
-      <S.IconWrapper onClick={() => setIsOpenMenu(true)}>
-        <MenuIcon aria-label="open menu" />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpenMenu(true)}>
+          <MenuIcon aria-label="open menu" />
+        </S.IconWrapper>
+      </MediaMatch>
       <S.LogoWrapper>
         <Logo $hideOnMobile={true} aria-label="won games" />
       </S.LogoWrapper>
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="search" />
@@ -30,6 +39,11 @@ const Menu = ({ userName }: MenuProps) => {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="open shopping cart" />
         </S.IconWrapper>
+        {!userName && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpenMenu} isOpenMenu={isOpenMenu}>
         <CloseIcon
