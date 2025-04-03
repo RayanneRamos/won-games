1e8a2d0ed54cb36a8c31559b185dda27559b9542
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-04-03 16:34:48 -0300

    menu parte 6: criando animações para o menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -91,6 +91,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     right: 0;
     height: 100vh;
     overflow: hidden;
+    transition: opacity 0.3s ease-in-out;
     opacity: ${isOpenMenu ? 1 : 0};
     pointer-events: ${isOpenMenu ? 'all' : 'none'};
 
@@ -117,6 +118,13 @@ export const MenuFull = styled.nav<MenuFullProps>`
       font-weight: ${theme.font.bold};
       font-size: ${theme.font.sizes.xlarge};
       margin-bottom: ${theme.spacings.small};
+      transform: ${isOpenMenu ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
+    }
+
+    ${RegisterBox} {
+      transform: ${isOpenMenu ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
     }
   `}
 `
