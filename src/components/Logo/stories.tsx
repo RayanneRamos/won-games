935c957e0eebfa923594a820d0fb50ddbf455b8e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-04-03 15:10:02 -0300

    menu parte 2: criando a versão mobile do menu

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,15 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Logo, { LogoProps } from '.'
 
-const meta: Meta<typeof Logo> = {
+export default {
   title: 'Logo',
   component: Logo
-}
+} as Meta
 
-export default meta
-
-type Story = StoryObj<typeof Logo>
-
-export const Default: Story = {
-  args: {}
-}
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />
