60e57035fa0b042baa4a4cff198ee6964ba2315a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-04-07 09:47:17 -0300

    banner parte 2: escrevendo testes

--- src/components/Banner/banner.spec.tsx
+++ src/components/Banner/banner.spec.tsx
@@ -1,13 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Banner from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+const props = {
+  image: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
 
 describe('<Banner />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Banner />)
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Banner/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new Crashlands season/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
   })
 })
